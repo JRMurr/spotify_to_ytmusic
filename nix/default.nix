c1a3ccae054e1d4bf92c3220f64331d58f079980
John Murray <5672686+JRMurr@users.noreply.github.com>
2025-08-12 12:57:16 -0400

    setup nix build

---
+++ nix/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  python3Packages,
+}:
+let
+
+  fs = lib.fileset;
+
+  baseDir = ../.;
+
+  python_files = fs.fileFilter (file: file.hasExt "py") baseDir;
+
+in
+
+python3Packages.buildPythonApplication {
+  pname = "spotify-to-ytmusic";
+  version = "1.11.0";
+
+  src = fs.toSource {
+    root = baseDir;
+    fileset = fs.unions [
+      python_files
+      ../pdm.lock
+      ../pyproject.toml
+    ];
+  };
+
+  pyproject = true;
+
+  build-system = with python3Packages; [
+    pdm-backend
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = with python3Packages; [
+    ytmusicapi
+    spotipy
+    platformdirs
+  ];
+
+}
